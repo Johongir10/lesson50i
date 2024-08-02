bb9dc80cad6f6a35bd8f82ca109de309b37a85db
Johongir10 <shavkatovj73@gmail.com>
2024-08-02 10:47:44 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,28 @@
+let person={
+    Name:"Johongir",
+    surName:"Shavkatov",
+    AGE:14,
+    height:1.71,
+    school:195,
+    Hobby:"IT",
+    Freinds:"Muxammataziz",
+    Amirxon:"Amirxon",
+    car:"BMW",
+    Woter:"Dinay",
+
+
+}
+console.log()
+
+for (const key in person) {
+   console.log(person[key])
+}
+
+// let meva=["olma","Anor","Banana","gilos"]
+
+
+// let oArray={}
+
+// for (const mevaa of meva) {
+//     console.log(mevaa)
+// }
